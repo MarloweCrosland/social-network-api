53a689ce72699584127494028e15e8339b7e052b
marlowe <marlowepdx@gmail.com>
2022-10-06 18:19:54 -0700

    add and remove friend functions created

--- Controllers/user-controller.js
+++ Controllers/user-controller.js
@@ -67,6 +67,34 @@ const userController = {
             })
             .catch((err) => res.status(400).json(err));
     },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+      // remove friend
+      removeFriend(req, res) {
+        User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
 };
 
 module.exports = userController;
